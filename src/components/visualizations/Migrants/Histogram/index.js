6a28860ac4e08d7fc52a37b57c60b8ffa08fe27d
Aga <aga@cottagelabs.com>
2023-12-29 22:18:38 +0000

    add histogram with binned data

--- src/components/visualizations/Migrants/Histogram/index.js
+++ src/components/visualizations/Migrants/Histogram/index.js
@@ -1,17 +1,18 @@
 import React, {useContext} from "react";
 import {AreaContext} from "../../../AreaContext";
-import {extent, scaleTime, scaleLinear, max, timeFormat, format} from "d3";
+import {extent, scaleTime, scaleLinear, max, timeFormat, format, bin, timeMonths, sum} from "d3";
 import {LinearAxisLeft} from "../../../shared/LinearAxisLeft";
 import {AxisBottom} from "../../../shared/AxisBottom";
+import {Marks} from "./Marks";
 
-const margin = { top: 20, right: 30, bottom: 65, left: 45 };
+const margin = {top: 20, right: 30, bottom: 65, left: 45};
 
 export const Histogram = ({data}) => {
     const context = useContext(AreaContext);
     const width = context.width;
-    const height = context.height*0.5;
-    const innerWidth = width-margin.left-margin.right;
-    const innerHeight = height-margin.bottom-margin.top;
+    const height = context.height * 0.5;
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.bottom - margin.top;
 
     const xAxisTickFormat = timeFormat('%m/%Y');
     const xValue = d => d.date;
@@ -22,8 +23,21 @@ export const Histogram = ({data}) => {
         .range([0, innerWidth])
         .nice();
 
+    const [start, stop] = xScale.domain();
+    const bins = bin()
+        .value(xValue)
+        .domain(xScale.domain())
+        .thresholds(timeMonths(start, stop))
+
+    const binnedData = bins(data).map(array => ({
+        y: sum(array, yValue),
+        x0: array.x0,
+        x1: array.x1
+    }));
+
+    const getSum = d => d.y
     const yScale = scaleLinear()
-        .domain([max(data, yValue),0])
+        .domain([max(binnedData, getSum), 0])
         .range([0, innerHeight])
         .nice();
 
@@ -39,6 +53,16 @@ export const Histogram = ({data}) => {
                     xScale={xScale}
                     innerHeight={innerHeight}
                     tickFormat={xAxisTickFormat}
+                />
+                <Marks
+                    data={binnedData}
+                    xScale={xScale}
+                    xValue={xValue}
+                    yScale={yScale}
+                    yValue={getSum}
+                    innerWidth={innerWidth}
+                    innerHeight={innerHeight}
+                    format={xAxisTickFormat}
                 />
             </g>
         </svg>
